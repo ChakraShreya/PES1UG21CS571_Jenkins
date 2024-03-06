c8f5ddafe53dd063bbaf80f26853522cf223e5e1
shreya <schakra1401@gmail.com>
2024-03-06 22:37:16 +0530

    updated

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,7 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout<< "update\n";
+  cout<< "updated\n";
   return 0;
 
 }
